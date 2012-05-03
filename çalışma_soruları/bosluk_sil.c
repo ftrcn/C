184e6d1ac9a2441094e0538877e6a14762324b8a
ftrcn <fatma.tercan@bil.omu.edu.tr>
2012-05-03 17:18:58 +0300

    .

---
+++ çalışma_soruları/bosluk_sil.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+
+    char dizi[100];
+    int i=0;
+    
+    puts("cumle giriniz:");
+    gets(dizi);
+    while(dizi[i]!='\0'){
+        if(isalnum(dizi[i])) 
+            putchar(dizi[i]);                
+        if(dizi[i]==' ' && dizi[i+1]!=' ')
+           putchar(dizi[i]);
+    i++;
+    } 
+    getch();
+    return 0;
+}
